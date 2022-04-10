7b1f39734e6a6731b87367d29eb171d1da59dad8
AaronMolesbury <55638411+AaronMolesbury@users.noreply.github.com>
2022-04-10 21:23:44 +0100

    Added Linkedin link

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,29 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../pages/Home.css";
+import { 
+  MDBBtn,
+  MDBIcon,
+  MDBNavbar,
+  MDBContainer,
+  MDBNavbarBrand, } from 'mdb-react-ui-kit';
 
 function Home() {
     return (
-        <div class="container-lg c1">
-          <p1 class="p1">
-            Junior Software Developer
-          </p1>
-          <h1 class = "h1">
-            Aaron Molesbury
-          </h1>
-          <br/>
-          <p2 class="p2">
-            Hi, I'm Aaron! I have recently graduated from Heriot Watt University 
-            with a degree <br/> in Computer Science and I'm looking to begin my Software Developer journey.
-            You can<br/> learn more about what I can offer below.
-          </p2>
-          <br/>
-          <br/>
-          <Link to="/about">
-            <button type="button" class="btn btn-dark btn-rounded" >
-              About Me
-            </button> 
-          </Link>
+        <div>
+          <div class="container-lg c1">
+            <p1 class="p1">
+              Junior Software Developer
+            </p1>
+            <h1 class = "h1">
+              Aaron Molesbury
+            </h1>
+            <br/>
+            <p2 class="p2">
+              Hi, I'm Aaron! I have recently graduated from Heriot Watt University 
+              with a degree <br/> in Computer Science and I'm looking to begin my Software Developer journey.
+              You can<br/> learn more about what I can offer below.
+            </p2>
+            <br/>
+            <br/>
+            <Link to="/about">
+              <button type="button" class="btn btn-dark btn-rounded" >
+                About Me
+              </button> 
+            </Link>
+          </div>
+          <MDBNavbar fixed='bottom'>
+            <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#dd4b39' }}>
+              <MDBIcon fab icon='google' size='lg' />
+            </MDBBtn>
+            <Link to={{ pathname: "www.linkedin.com/in/aaron-molesbury" }} target="_blank">
+              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#0082ca' }}>
+                <MDBIcon fab icon='linkedin' size='lg' />
+              </MDBBtn>
+            </Link>
+          </MDBNavbar>
         </div>
+        
+        
       );
 }
 
