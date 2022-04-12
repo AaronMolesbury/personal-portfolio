6f55f749f188bd554f63ab7de7f48c002971b32c
AaronMolesbury <55638411+AaronMolesbury@users.noreply.github.com>
2022-04-12 14:51:56 +0100

    Embedded CV into CV page

--- my-app/src/pages/CV.js
+++ my-app/src/pages/CV.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import "./CV.css";
 function CV() {
   return (
-    <body>
-      <p>cv</p>
-    </body>
+    <>
+      <iframe src="https://drive.google.com/file/d/1WAcQxD3NyQHl8Rmn-8eTST13VopdTx5X/preview#toolbar=0"
+        allow="autoplay"
+        class="iframe">
+      </iframe>
+    </>
   );
 }
 
