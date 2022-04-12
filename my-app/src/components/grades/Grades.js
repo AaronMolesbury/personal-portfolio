0d3f2584699aa70dabc8fb6a9cf4edd52009949d
AaronMolesbury <55638411+AaronMolesbury@users.noreply.github.com>
2022-04-12 16:51:38 +0100

    Began Education Section

---
+++ my-app/src/components/grades/Grades.js
@@ -0,0 +1,146 @@
+function Grades(){
+    return(
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <p class="li-header">
+                        Year 1
+                    </p>
+                    <li class="ali">
+                        Logic and Proof (A)
+                    </li>
+                    <li class="ali">
+                        Interactive Systems (A)
+                    </li>
+                    <li class="ali">
+                        Praxis (B)
+                    </li>
+                    <li class="ali">
+                        Software Development 1 (A)
+                    </li>
+                    <li class="ali">
+                        Software Development 2 (A)
+                    </li>
+                    <li class="ali">
+                        Software Development 3 (A)
+                    </li>
+                    <li class="ali">
+                        Introduction to Computer Systems (A)
+                    </li>
+                    <li class="ali">
+                        Web Design and Databases (A)
+                    </li>
+                </div>
+                <div class="col-md">
+                    <p class="li-header">
+                        Year 2
+                    </p>
+                    <li class="ali">
+                        Data Structures and Algorithms (B)
+                    </li>
+                    <li class="ali">
+                        User-Centred Experimental Design (A)
+                    </li>
+                    <li class="ali">
+                        Programming Languages (B)
+                    </li>
+                    <li class="ali">
+                        Web Programming (B)
+                    </li>
+                    <li class="ali">
+                        Discrete Mathematics (P)*
+                    </li>
+                    <li class="ali">
+                        Database Management Systems (P)*
+                    </li>
+                    <li class="ali">
+                        Hardware-Software (P)*
+                    </li>
+                    <li class="ali">
+                        Software Design (P)*
+                    </li>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md">
+                    <p class="li-header">
+                        Year 3
+                    </p>
+                    <li class="ali">
+                        Foundations 1 (A)
+                    </li>
+                    <li class="ali">
+                        Artificial Intelligence and Language Processors (A)
+                    </li>
+                    <li class="ali">
+                        Data Communications and Network Systems (B)
+                    </li>
+                    <li class="ali">
+                        Software Engineering (A)
+                    </li>
+                    <li class="ali">
+                        Foundations 2 (A)
+                    </li>
+                    <li class="ali">
+                        Language Processors (A)
+                    </li>
+                    <li class="ali">
+                        Operating Systems and Concurrency (A)
+                    </li>
+                    <li class="ali">
+                        Professional Development (A)
+                    </li>
+                </div>
+                <div class="col-md">
+                    <p class="li-header">
+                        Year 4
+                    </p>
+                    <li class="ali">
+                        Biologically Inspired Computation (B)
+                    </li>
+                    <li class="ali">
+                        Rigorous Methods of Software Engineering (A)
+                    </li>
+                    <li class="ali">
+                        3D Graphics and Animations (A)
+                    </li>
+                    <li class="ali">
+                        Computer Games Programming (A)
+                    </li>
+                    <li class="ali">
+                        E-Commerce Technologies (TBA)
+                    </li>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md">
+                    <p class="li-header">
+                        Dissertation Project
+                    </p>
+                    <li class="ali">
+                        Research Methods and Requirements Engineering (A)
+                    </li>
+                    <li class="ali">
+                        Design and Implementation (TBA)
+                    </li>
+                    <li class="ali">
+                        Project Testing and Presentation (TBA)
+                    </li>
+                </div>
+                <div class="col-md">
+                    <p class="li-header">
+                        Awards
+                    </p>
+                    <li class="ali">
+                        Deputy Principles Award 2018/19 <br/>(Awarded for Attaining 6+ A grades over a year)
+                    </li>
+                    <li class="ali">
+                        Deputy Principles Award 2020/21
+                    </li>
+                </div>
+            </div>  
+        </div>  
+      )
+}
+
+export default Grades;
--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import "./About.css";
+import Grades from "../components/grades/Grades"
 
 function About() {
   return (
-    null
+    <>
+      <div class="about-header">
+        <h1 class="ah1">
+          About Me
+        </h1>
+      </div>
+      <div class="about-body">
+        <h2 class="ah2">
+          Educational Attainment
+        </h2>
+        <p class="ap1">
+          For my degree, I studied a BSc in Computer Science at Heriot-Watt University in Edinburgh. The following
+          breakdown showcases the grades i attained per year including any awards achieved on top.
+        </p>
+        <Grades/>
+         
+        
+      </div>
+    </>
   );
 }
 
